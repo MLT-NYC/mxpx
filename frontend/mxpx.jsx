3c8a116417bc8fe12b2674c76c6eaf3b4eb3590e
MLT-NYC <maxtrukhin@gmail.com>
2019-03-26 16:49:24 -0400

    Configured PackageJSON

---
+++ frontend/mxpx.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+document.addEventListener('DOMContentLoaded', () => {
+    let root = document.getElementById('root');
+    // let store = configureStore();
+    ReactDOM.render(<h1>mxpx.nyc</h1>, root)
+});
