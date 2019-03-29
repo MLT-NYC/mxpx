fd2d97a319d31cb0fc17c738f18ae554421c8ed0
MLT-NYC <maxtrukhin@gmail.com>
2019-03-29 16:58:17 -0400

    Additional styling to splash page

--- frontend/components/welcome/welcome.jsx
+++ frontend/components/welcome/welcome.jsx
@@ -19,27 +19,37 @@ class Welcome extends React.Component {
         } else {
             return (
                 <>
-                    
-                    <nav className='navBar'>
-                        
-                        <div className='nav-left'>
+                    <div className='welcomeHeader'> 
+                        <div className='headerImage'>
+                        </div>
+
+                        <nav className='navBar'>
                             <h1 className='logo'>mxpx</h1>
-                            <div className='navBar-splash'>Discover</div>
-                            <div className='navBar-splash'>Licensing</div>
-                            <div className='navBar-splash'>Memberships</div>
-                            <div className='navBar-splash'>Quests</div>
-                            <div className='navBar-splash'>Studio</div>
-                            <div className='navBar-splash'>Blog</div>
+                            <div className='nav-left'>
+                                <div className='navBar-splash-item'>Discover</div>
+                                <div className='navBar-splash-item'>Licensing</div>
+                                <div className='navBar-splash-item'>Memberships</div>
+                                <div className='navBar-splash-item'>Quests</div>
+                                <div className='navBar-splash-item'>Studio</div>
+                                <div className='navBar-splash-item'>Blog</div>
+                            </div>
+
+                            <div className='nav-right'>
+                                <div className='welcomeButtons'>
+                                    <Link to={'/login'} className='logInButton'>Log in</Link>
+                                    <Link to={'/signup'} className='signUpButton'>Sign up</Link>
+                                </div>
+                            </div>
+ 
+                        </nav>
+
+                        <div className="welcomeMessages">
+                            <h1 className='titleMessage'>Get inspired and share your best photos</h1>
+                            <h3 className='subtitleMessage'>Find your home among the world's best photographers</h3>
+                            <Link to={'/login'} className='joinButton'>Join mxpx</Link>
                         </div>
-
-                        <div className='nav-right'>
-                            <Link to={'/login'} className='logInButton'>Log in</Link>
-                            <Link to={'/signup'} className='signUpButton'>Sign up</Link>
-                        </div>
-
-                    </nav>
-
-                    <div className='headerImage'></div>
+                    </div>
+                    
 
                 </>
             )
