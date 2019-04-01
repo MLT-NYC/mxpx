f23638ea2d1c1a80808b548d04bfffc052916068
MLT-NYC <maxtrukhin@gmail.com>
2019-04-01 19:48:46 -0400

    Created pictures utils and actions

---
+++ frontend/actions/pictures_actions.js
@@ -0,0 +1,58 @@
+import * as PictureApiUtils from '../util/pictures_api_util';
+
+export const RECEIVE_ALL_PICTURES = 'RECEIVE_ALL_PICTURES';
+export const RECEIVE_PICTURE = 'RECEIVE_PICTURE';
+export const REMOVE_PICTURE = 'REMOVE_PICTURE';
+export const RECEIVE_PICTURE_ERRORS = 'RECEIVE_PICTURE_ERRORS';
+export const CLEAR_PICTURE_ERRORS = 'CLEAR_PICTURE_ERRORS';
+
+const receivePictures = pictures => ({
+    type: RECEIVE_ALL_PICTURES,
+    pictures: pictures
+});
+
+const receivePicture = picture => ({
+    type: RECEIVE_PICTURE,
+    picture: picture
+});
+
+const removePicture = picture => ({
+    type: REMOVE_PICTURE,
+    pictureId: picture.id
+});
+
+const receivePictureErrors = (errors) => ({
+    type: RECEIVE_PICTURE_ERRORS,
+    errors: errors
+});
+
+export const clearPictureErrors = () => ({
+    type: CLEAR_PICTURE_ERRORS,
+});
+
+export const fetchPictures = () => dispatch => {
+    return PictureApiUtils.fetchPictures()
+        .then(pictures => dispatch(receivePictures(pictures)));
+};
+
+export const fetchPicture = (id) => dispatch => {
+    return PictureApiUtils.fetchPicture(id)
+        .then(picture => dispatch(receivePicture(picture)));
+};
+
+export const createPicture = (picture) => dispatch => {
+    return PictureApiUtils.createPicture(picture)
+        .then(picture => dispatch(receivePicture(picture)),
+        errors => dispatch(receivePictureErrors(errors)));
+};
+
+export const updatePicture = (picture) => dispatch => {
+    return PictureApiUtils.updatePicture(picture)
+        .then(picture => dispatch(receivePicture(picture)),
+            errors => dispatch(receivePictureErrors(errors)));
+};
+
+export const deletePicture = (picture) => dispatch => {
+    return PictureApiUtils.deletePicture(picture)
+        .then(picture => dispatch(removePicture(picture)));
+};
---
+++ frontend/util/pictures_api_util.js
@@ -0,0 +1,36 @@
+export const fetchPictures = () => {
+    return $.ajax({
+        method: 'GET',
+        url: `/api/pictures`
+    });
+};
+
+export const fetchPicture = (id) => {
+    return $.ajax({
+        method: 'GET',
+        url: `/api/pictures/${id}`
+    });
+};
+
+export const createPicture = (picture) => {
+    return $.ajax({
+        method: 'POST',
+        url: `/api/pictures`,
+        data: { picture }
+    });
+};
+
+export const updatePicture = (picture) => {
+    return $.ajax({
+        method: 'PATCH',
+        url: `/api/pictures/${picture.id}`,
+        data: { picture }
+    });
+};
+
+export const deletePicture = (id) => {
+    return $.ajax({
+        method: 'DELETE',
+        url: `/api/pictures/${id}`
+    });
+};
