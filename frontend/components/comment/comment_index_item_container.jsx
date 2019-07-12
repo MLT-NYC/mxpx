9031e7f42d7f7c743275442f362448bd0580cf09
MLT-NYC <maxtrukhin@gmail.com>
2019-07-12 17:26:48 -0400

    Created commentIndexItem/Container components

---
+++ frontend/components/comment/comment_index_item_container.jsx
@@ -0,0 +1,16 @@
+import { connect } from 'react-redux';
+import CommentIndexItem from './comment_index_item';
+
+const mapStateToProps = state => {
+    return ({
+
+    });
+};
+
+const mapDispatchToProps = dispatch => {
+    return ({
+
+    });
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(CommentIndexItem);
