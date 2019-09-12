b11b9ff47dd4542fe9973d4043cfc0fa38b1c24f
MLT-NYC <maxtrukhin@gmail.com>
2019-09-11 23:28:35 -0400

    Created new navBar component

---
+++ frontend/components/navbar/nav_bar.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+class NavBar extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+
+        }
+    }
+
+    render() {
+        return(
+            <div className='navBar'>
+
+            </div>
+        )
+    }
+}
+
+export default NavBar;
---
+++ frontend/components/navbar/nav_bar_container.jsx
@@ -0,0 +1,16 @@
+import { connect } from 'react-redux';
+import NavBar from "./nav_bar";
+
+const mapStateToProps = props => {
+    return ({
+
+    })
+};
+
+const mapDispatchToProps = dispatch => {
+    return ({
+
+    })
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(NavBar);
